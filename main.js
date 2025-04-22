ecc8066a5687636cdaa0b4225013351933391454
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-04-22 16:44:55 -0500

    feat: :sparkles: Galeria de Pokemones terminada

---
+++ main.js
@@ -0,0 +1,38 @@
+const pokemon_name = document.getElementById("name-pokemon");
+const pokemon_url = document.getElementById("url-pokemon");
+const add_pokemon = document.getElementById("add-pokemon");
+const pokemon_card = document.getElementsByClassName("card");
+
+add_pokemon.addEventListener("click", (e) => {
+    e.preventDefault();
+    let pokemon = document.createElement("div");
+    pokemon.classList.add("card");
+    pokemon.style.display = "inline-block";
+    pokemon.innerHTML = `
+        <img src="${pokemon_url.value}" alt="pokemon">
+        <p>${pokemon_name.value}</p>
+        <button class="button">
+            <span class="X"></span>
+            <span class="Y"></span>
+          </button>
+    `;
+
+pokemon.addEventListener('dblclick', () => {
+    let newUrl = prompt("Ingresa la nueva URL de la imagen del Pokémon");
+    let img = pokemon.querySelector('img');
+    if (newUrl) {
+        img.src = newUrl;
+    }
+});
+
+let close_button = pokemon.querySelector(".button");
+close_button.addEventListener("click", () => {
+    pokemon.remove(); 
+
+});
+
+document.body.appendChild(pokemon);
+})
+
+
+
